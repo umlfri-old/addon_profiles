c394b1953ca998b37aab56e6ed7e894b374cf29d
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-22 11:49:46 +0000

    Added tags to CStereotype class.

--- profiles/trunk/plugin/profiles/Stereotype.py
+++ profiles/trunk/plugin/profiles/Stereotype.py
@@ -1,10 +1,15 @@
 class CStereotype(object):
-    def __init__(self, stereotypeElement, parentStereotype=None):
+    def __init__(self, stereotypeElement, tags, parentStereotype=None):
         self.stereotypeElement = stereotypeElement
+        self.tags = tags
         self.parentStereotype = parentStereotype
 
     def GetStereotypeElement(self):
         return self.stereotypeElement
+
+    def GetTags(self):
+        for tag in self.tags:
+            yield tag
 
     def GetName(self):
         return self.stereotypeElement.name
