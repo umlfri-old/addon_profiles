b4c9e1d849fed2050ff63db671357fceaef701a8
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-22 20:08:04 +0000

    Added GetAppliedProfiles to CProfileManager, which retrieves applied profiles from package element.

--- profiles/trunk/plugin/profiles/ProfileManager.py
+++ profiles/trunk/plugin/profiles/ProfileManager.py
@@ -1,13 +1,28 @@
 from ProfilePackageProcessor import CProfilePackageProcessor
+from ProfileApplicationExtractor import CProfileApplicationExtractor
+from ElementTypes import KnownElementTypes
 
 
 class CProfileManager(object):
 
     __profilePackageProcessor = CProfilePackageProcessor()
+    __profileApplicationExtractor = CProfileApplicationExtractor()
 
     def GetAvailableProfiles(self, packageElement):
         profilePackages = self.__profilePackageProcessor.ProcessPackage(packageElement)
         return {p.GetUID(): p for p in profilePackages}
+
+    def GetAppliedProfiles(self, package, availableProfiles):
+        applications = self.__profileApplicationExtractor.ExtractApplications(package)
+
+        appliedProfiles = []
+        for application in applications:
+            if application.GetProfilePackageID() in availableProfiles:
+                appliedProfiles.append(availableProfiles[application.GetProfilePackageID()])
+            else:
+                appliedProfiles.append(application.GetProfileName())
+
+        return appliedProfiles
 
     def IsPackage(self, element):
         return element.type.name == KnownElementTypes.PackageType
