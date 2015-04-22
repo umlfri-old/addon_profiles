990d49134c56ee1c48f2389adfffd0bc5e1b3621
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-22 11:49:45 +0000

    Added name and type property to CTag class.

--- profiles/trunk/plugin/profiles/Tag.py
+++ profiles/trunk/plugin/profiles/Tag.py
@@ -1,3 +1,10 @@
 class CTag(object):
-    def __init__(self):
-        pass
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def GetName(self):
+        return self.name
+
+    def GetType(self):
+        return self.type
