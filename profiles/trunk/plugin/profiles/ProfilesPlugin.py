edc798c9d6e947817075745375655d207a551c1f
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-22 09:34:23 +0000

    Added menu item to change applied profiles into main toolbar, which is disabled when non-UML project is opened.

--- profiles/trunk/plugin/profiles/ProfilesPlugin.py
+++ profiles/trunk/plugin/profiles/ProfilesPlugin.py
@@ -2,3 +2,29 @@ class CProfilesPlugin(object):
 
     def __init__(self, interface):
         self.__interface = interface
+
+        self.__interface.add_notification('project-opened', self.__OnProjectOpened)
+
+        self.__CreateToolbarButtons()
+        self.__UpdateButtonVisibility()
+
+    def __OnProjectOpened(self):
+        self.__UpdateButtonVisibility()
+
+    def __UpdateButtonVisibility(self):
+        project = self.__interface.project
+        if project is not None and project.metamodel.uri == "urn:umlfri.org:metamodel:uml":
+            self.__changeAppliedProfilesButton.visible = True
+        else:
+            self.__changeAppliedProfilesButton.visible = False
+
+    def __CreateToolbarButtons(self):
+        self.__changeAppliedProfilesButton = self.__interface.gui_manager.button_bar.add_button(
+            'ChangeAppliedProfiles',
+            lambda *a: self.__ShowProfileListDialog(),
+            -1,
+            'Profiles'
+        )
+
+    def __ShowProfileListDialog(self):
+        pass
