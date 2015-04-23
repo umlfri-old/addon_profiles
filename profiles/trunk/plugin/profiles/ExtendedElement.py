2a2444be67acda1a02a70ec20bf04e42ee84fcd7
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-23 17:11:30 +0000

    Implemented __hash__, __cmp__, __eq__ and __ne__ in CExtendedElement.

--- profiles/trunk/plugin/profiles/ExtendedElement.py
+++ profiles/trunk/plugin/profiles/ExtendedElement.py
@@ -1,9 +1,26 @@
 class CExtendedElement(object):
     def __init__(self, elementType):
         self.elementType = elementType
+        self.elementTypeName = self.elementType.name
 
     def GetElementType(self):
         return self.elementType
 
     def GetElementDomain(self):
         return self.elementType.domain
+
+    def __hash__(self):
+        return self.elementTypeName
+
+    def __cmp__(self, other):
+        if other is None:
+            return -1
+        return self.elementTypeName.__cmp__(other.elementTypeName)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.elementTypeName == other.elementTypeName
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
