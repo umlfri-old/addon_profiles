d9b2d2f72caf7b96816fbf98bdfe5c0a342ffd0e
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-23 11:37:08 +0000

    CAppliedProfileEditor discovers applied profiles in project and passes them into CProfileListDialog.

--- profiles/trunk/plugin/profiles/AppliedProfileEditor.py
+++ profiles/trunk/plugin/profiles/AppliedProfileEditor.py
@@ -1,10 +1,12 @@
 from ProfileManager import CProfileManager
+from ProfileApplicationDiscovery import CProfileApplicationDiscovery
 from gui import CProfileListDialog
 
 
 class CAppliedProfileEditor(object):
 
     __profileManager = CProfileManager()
+    __profileApplicationDiscovery = CProfileApplicationDiscovery()
 
     def __init__(self, interface):
         self.__interface = interface
@@ -12,4 +14,12 @@ class CAppliedProfileEditor(object):
     def EditProfiles(self):
         projectRoot = self.__interface.project.root
         availableProfiles = self.__profileManager.GetAvailableProfiles(projectRoot)
-        CProfileListDialog(projectRoot, availableProfiles.itervalues()).Show()
+
+        profileApplications = self.__profileApplicationDiscovery.DiscoverProfileApplications(projectRoot)
+        appliedProfiles = self.__GetAppliedProfiles(profileApplications, availableProfiles)
+
+        CProfileListDialog(projectRoot, dict(appliedProfiles), availableProfiles.itervalues()).Show()
+
+    def __GetAppliedProfiles(self, profileApplications, availableProfiles):
+        return {package: self.__profileManager.GetAppliedProfiles(applications, availableProfiles)
+                for package, applications in profileApplications.iteritems()}
