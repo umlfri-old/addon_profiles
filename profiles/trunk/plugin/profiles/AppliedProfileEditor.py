f5d8ede81d6b6ae176fddd4df691204515d91f11
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-23 11:57:31 +0000

    CAppliedProfileEditor now uses CApplyProfilesTransaction to retrieve input parameters for profile list dialog.

--- profiles/trunk/plugin/profiles/AppliedProfileEditor.py
+++ profiles/trunk/plugin/profiles/AppliedProfileEditor.py
@@ -1,25 +1,17 @@
-from ProfileManager import CProfileManager
-from ProfileApplicationDiscovery import CProfileApplicationDiscovery
 from gui import CProfileListDialog
+from ApplyProfilesTransaction import CApplyProfilesTransaction
 
 
 class CAppliedProfileEditor(object):
-
-    __profileManager = CProfileManager()
-    __profileApplicationDiscovery = CProfileApplicationDiscovery()
 
     def __init__(self, interface):
         self.__interface = interface
 
     def EditProfiles(self):
         projectRoot = self.__interface.project.root
-        availableProfiles = self.__profileManager.GetAvailableProfiles(projectRoot)
 
-        profileApplications = self.__profileApplicationDiscovery.DiscoverProfileApplications(projectRoot)
-        appliedProfiles = self.__GetAppliedProfiles(profileApplications, availableProfiles)
+        applyProfilesTransaction = CApplyProfilesTransaction(projectRoot)
 
-        CProfileListDialog(projectRoot, dict(appliedProfiles), availableProfiles.itervalues()).Show()
-
-    def __GetAppliedProfiles(self, profileApplications, availableProfiles):
-        return {package: self.__profileManager.GetAppliedProfiles(applications, availableProfiles)
-                for package, applications in profileApplications.iteritems()}
+        appliedProfiles = applyProfilesTransaction.GetCurrentProfiles()
+        availableProfiles = applyProfilesTransaction.GetAvailableProfiles()
+        CProfileListDialog(projectRoot, appliedProfiles, availableProfiles).Show()
