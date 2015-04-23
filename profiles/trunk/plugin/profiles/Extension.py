054b4b83ea84e8996e28cb5e4df434bcda53361c
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-23 14:36:00 +0000

    Created CExtension class representing extension connection between metaclass and stereotype.

---
+++ profiles/trunk/plugin/profiles/Extension.py
@@ -0,0 +1,7 @@
+class CExtension(object):
+
+    def __init__(self, extendedElement):
+        self.extendedElement = extendedElement
+
+    def GetExtendedElement(self):
+        return self.extendedElement
