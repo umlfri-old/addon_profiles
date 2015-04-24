d45747e28b6617afdbd91e3e9eda4dd0dfd763d0
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-24 18:53:49 +0000

    Moved attribute identifiers of profile application into KnownAttributes enum class.

--- profiles/trunk/plugin/profiles/ProfileApplicationExtractor.py
+++ profiles/trunk/plugin/profiles/ProfileApplicationExtractor.py
@@ -1,13 +1,17 @@
 from ast import literal_eval
 from ProfileApplication import CProfileApplication
+from DomainTypes import KnownAttributes
 
 
 class CProfileApplicationExtractor(object):
 
     def ExtractApplications(self, packageElement):
-        rawListOfProfiles = packageElement.values['applied_profiles']
+        rawListOfProfiles = packageElement.values[KnownAttributes.AppliedProfiles]
         listOfProfiles = literal_eval(rawListOfProfiles)
         return [self.__CreateApplication(props) for props in listOfProfiles]
 
     def __CreateApplication(self, props):
-        return CProfileApplication(props['package_id'], props['profile_name'], props['modification_bundle'])
+        return CProfileApplication(
+            props[KnownAttributes.ProfileApplication.PackageID],
+            props[KnownAttributes.ProfileApplication.ProfileName],
+            props[KnownAttributes.ProfileApplication.ModificationBundle])
