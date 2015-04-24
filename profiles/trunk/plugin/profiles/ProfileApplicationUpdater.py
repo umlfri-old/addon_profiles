c8e246580883fc2f292ddcacf8b7a0922f6fc921
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-24 21:47:07 +0000

    Implemented removing profile applications from package element after the profile has been removed.

--- profiles/trunk/plugin/profiles/ProfileApplicationUpdater.py
+++ profiles/trunk/plugin/profiles/ProfileApplicationUpdater.py
@@ -1,6 +1,12 @@
+from ast import literal_eval
 from DomainTypes import KnownAttributes
 from DomainObjectHelper import AppendItemsIntoList
 from ProfileApplicationMapper import CProfileApplicationMapper
+
+
+class ProfileApplicationNotFound(Exception):
+    def __init__(self, profileApplication):
+        self.profileApplication = profileApplication
 
 
 class CProfileApplicationUpdater(object):
@@ -10,3 +16,23 @@ class CProfileApplicationUpdater(object):
     def AddProfileApplications(self, element, profileApplications):
         items = [self.__profileApplicationMapper.ConvertToProperties(a) for a in profileApplications]
         AppendItemsIntoList(element, KnownAttributes.AppliedProfiles, items)
+
+    def RemoveProfileApplications(self, element, profileApplications):
+        values = literal_eval(element.values[KnownAttributes.AppliedProfiles])
+        indexes = self.__FindIndexes(values, profileApplications)
+        for index, application in indexes:
+            path = '{0}[{1}]'.format(KnownAttributes.AppliedProfiles, index)
+            element.remove_item(path)
+
+    def __FindIndexes(self, values, profileApplications):
+        for index, item in enumerate(values):
+            application = self.__GetProfileApplication(item, profileApplications)
+            if application is None:
+                raise ProfileApplicationNotFound(application)
+
+            yield index, application
+
+    def __GetProfileApplication(self, item, profileApplications):
+        for application in profileApplications:
+            if item[KnownAttributes.ProfileApplication.PackageID] == application.GetProfilePackageID():
+                return application
