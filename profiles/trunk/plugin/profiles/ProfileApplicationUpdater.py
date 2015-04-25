de328af71e17078b559dfb1e6ac6b16887c398da
louma <louma@d6d51e68-c4c5-48db-96ef-4d935770b557>
2015-04-25 17:37:41 +0000

    Replaced incorrect algorithm for mapping indexes in list of profile application properties to CApplicationProfile objects with simpler algorithm for removing profile applications from package element that works.

--- profiles/trunk/plugin/profiles/ProfileApplicationUpdater.py
+++ profiles/trunk/plugin/profiles/ProfileApplicationUpdater.py
@@ -3,6 +3,12 @@ from DomainTypes import KnownAttributes
 from DomainObjectHelper import AppendItemsIntoList
 from ProfileApplicationMapper import CProfileApplicationMapper
 
+def enumerate_down(l, start=None, step=-1):
+    if start is None:
+        start = len(l) - 1
+    for index, item in zip(xrange(start, -1, step), l.__reversed__()):
+        yield index, item
+
 
 class ProfileApplicationNotFound(Exception):
     def __init__(self, profileApplication):
@@ -19,21 +25,14 @@ class CProfileApplicationUpdater(object):
 
     def RemoveProfileApplications(self, element, profileApplications):
         values = literal_eval(element.values[KnownAttributes.AppliedProfiles])
-        indexes = self.__FindIndexes(values, profileApplications)
-        indexes = sorted(indexes, key=lambda x: x[0], reverse=True)
-        for index, application in indexes:
-            path = '{0}[{1}]'.format(KnownAttributes.AppliedProfiles, index)
-            element.remove_item(path)
 
-    def __FindIndexes(self, values, profileApplications):
-        for index, item in enumerate(values):
-            application = self.__GetProfileApplication(item, profileApplications)
-            if application is None:
-                raise ProfileApplicationNotFound(application)
+        applicationsDict = {a.GetProfilePackageID(): a for a in profileApplications}
+        for index, item in enumerate_down(values):
+            profilePackageID = item[KnownAttributes.ProfileApplication.PackageID]
+            wasRemoved = applicationsDict.pop(profilePackageID, None)
+            if wasRemoved is not None:
+                path = '{0}[{1}]'.format(KnownAttributes.AppliedProfiles, index)
+                element.remove_item(path)
 
-            yield index, application
-
-    def __GetProfileApplication(self, item, profileApplications):
-        for application in profileApplications:
-            if item[KnownAttributes.ProfileApplication.PackageID] == application.GetProfilePackageID():
-                return application
+            if len(applicationsDict) == 0:
+                break
